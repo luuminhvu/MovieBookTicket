ac963b2d3ce82fb3e76bcd87af4bf5e5358bfe4e
Luu Vu <vu7178@gmail.com>
2024-04-02 22:35:49 +0700

    fix security cloudinary

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,9 +1,10 @@
 const cloudinary = require("cloudinary");
-
+const dotenv = require("dotenv");
+dotenv.config();
 cloudinary.v2.config({
-  cloud_name: "djicwnp8t",
-  api_key: "488843923552337",
-  api_secret: "LSTGulwKp5ptvz_yQITHSh39G0E",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
   secure: true,
 });
 module.exports = cloudinary;
